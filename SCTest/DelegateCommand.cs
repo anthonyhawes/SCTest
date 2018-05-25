4bf56b84c2d23cc182bb40fc40f1740c477a8fdd
Anthony Hawes <anthony@cortexsoftware.co.uk>
2018-05-25 17:11:40 +0100

    Basic app to run sclang and evaluate commands

---
+++ SCTest/DelegateCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SCTest
+{
+    class DelegateCommand : ICommand
+    {
+        private readonly Action action;
+
+        public DelegateCommand(Action action)
+        {
+            this.action = action;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            action?.Invoke();
+        }
+    }
+}
---
+++ SCTest/SCViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SCTest
+{
+    class SCViewModel : BaseViewModel
+    {
+        private Process process;
+        private string text;
+
+        public SCViewModel()
+        {
+            ClientCommand = new DelegateCommand(DoClientCommand);
+            ClientOutput = new ObservableCollection<string>();
+            EvalCommand = new DelegateCommand(DoEvalCommand);
+        }
+
+        public string ClientText
+        {
+            get { return process == null ? "Start" : "Stop"; }
+        }
+
+        public ICommand ClientCommand { get; }
+
+        public ObservableCollection<string> ClientOutput { get; }
+
+        private void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() => ClientOutput.Add(e.Data));
+        }
+
+        public ICommand EvalCommand { get; }
+
+        public string EvalText
+        {
+            get { return text; }
+            set { SetProperty(ref text, value); }
+        }
+
+        private void Process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() => ClientOutput.Add(e.Data));
+        }
+
+        private void DoClientCommand()
+        {
+            if (process == null)
+            {
+                var folder = ConfigurationManager.AppSettings["SCFolder"];
+                var startInfo = new ProcessStartInfo();
+                startInfo.FileName = Path.Combine(folder, "sclang.exe");
+                startInfo.WorkingDirectory = folder;
+                startInfo.CreateNoWindow = true;
+                startInfo.UseShellExecute = false;
+                startInfo.RedirectStandardError = true;
+                startInfo.RedirectStandardInput = true;
+                startInfo.RedirectStandardOutput = true;
+                startInfo.StandardErrorEncoding = Encoding.UTF8;
+                startInfo.StandardOutputEncoding = Encoding.UTF8;
+                process = Process.Start(startInfo);
+                process.ErrorDataReceived += Process_ErrorDataReceived;
+                process.OutputDataReceived += Process_OutputDataReceived;
+                process.BeginErrorReadLine();
+                process.BeginOutputReadLine();
+            }
+            else
+            {
+                process.StandardInput.Write("0.exit");
+                process.StandardInput.Write('\x1b');
+                if (!process.WaitForExit(1000))
+                {
+                    process.Kill();
+                    process.WaitForExit(500);
+                }
+                process.ErrorDataReceived -= Process_ErrorDataReceived;
+                process.OutputDataReceived -= Process_OutputDataReceived;
+                process = null;
+            }
+            OnPropertyChanged("ClientText");
+        }
+
+        private void DoEvalCommand()
+        {
+            if (process != null && !string.IsNullOrEmpty(text))
+            {
+                process.StandardInput.Write(text);
+                process.StandardInput.Write('\x0c');
+            }
+        }
+    }
+}
